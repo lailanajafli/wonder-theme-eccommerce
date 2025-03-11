ebda807b90a71eaae7172fa669b335c76dc5307a
lailanajafli <laila.nac2001@gmail.com>
2025-03-12 01:30:02 +0400

    Scroll up when the location changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import AppRouter from "./router/AppRouter";
 import {Provider} from 'react-redux';
 import store from './redux/store/store';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Provider store={store}>
+       <ScrollToTop />
     <AppRouter />
     </Provider>
   )
