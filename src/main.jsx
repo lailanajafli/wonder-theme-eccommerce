12e63c9afef41632878d46f097404fd4485bb8b0
lailanajafli <laila.nac2001@gmail.com>
2025-04-02 19:36:20 +0400

    removed index.scss

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import App from './App.jsx'
 // Router
 import { BrowserRouter } from "react-router-dom";
 
-// Styles
-import "src/assets/scss/index.scss";
+
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
